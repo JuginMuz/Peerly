9aa4226e7671f5a11a66693589cc5f26bbc958de
KumarS14 <sonjaykumar1404@gmail.com>
2025-03-02 14:24:51 +0000

    route that returns a single user with error checking and validation alongside a parameterized sql query for sec purposes

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -18,6 +18,27 @@ router.get('/', async (req, res) => { //route for getting all users which is asy
         res.status(500).json({ error: err.message });  // Show real error
     }
 });
+router.get('/:id', async (req, res) => { //route to get specific user by id
+    try {
+        const sql = 'SELECT * FROM users WHERE user_id = ?'; // sql query that will select the whole list of users but
+        //only return the first user that has the specified id. the sql query is also parameterized 
+        //in order to prevent a sql injection 
+        const user_id = req.params.id; // I had a issue here where i wrote user_id instead of id
+        // but I found out that since express does not automatically extract parameters based on your query string variable names
+        // i had to replace it with id instead 
+        const [users] =  await db.query(sql, [user_id]); //calls db and runs a query with the sql and userid
+        //however the user id is passed as a array becuase msql expects parameters in a array
+        if (users.length === 0) { // basic valadation to find out if the query is looking for a user 
+            //that does not exist
+            console.log("User not found!"); // Log if the user isn't in the database
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.json(users[0]);
+    }
+    catch(err) {
+        console.error("Query Failed:" , err.message);
+        res.status(500).json({error: err.message })
+    }
 
-
+});
 module.exports = router;
