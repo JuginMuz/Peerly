7716759f2de0f13dbade780ae1d6518a37d6a8e1
KumarS14 <sonjaykumar1404@gmail.com>
2025-03-12 17:32:38 +0000

    joined user and post so user details can be shown when looking at a post

--- app/models/Post.js
+++ app/models/Post.js
@@ -2,14 +2,25 @@
 const pool = require('./db');
 
 class Post {
-  constructor({ post_id, user_id, description, media_url, created_at }) {
+  constructor({
+    post_id,
+    user_id,
+    description,
+    media_url,
+    created_at,
+    first_name,      //user details
+    last_name,       
+    profile_picture, 
+  }) {
     this.post_id = post_id;
     this.user_id = user_id;
     this.description = description;
     this.media_url = media_url;
     this.created_at = created_at;
+    this.first_name = first_name;     
+    this.last_name = last_name;
+    this.profile_picture = profile_picture; 
   }
-
 }
 
 module.exports = Post;
