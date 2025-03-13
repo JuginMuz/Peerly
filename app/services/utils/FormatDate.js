44a6392312330be0ccf332fa772de24f0c98f402
KumarS14 <sonjaykumar1404@gmail.com>
2025-03-13 11:57:31 +0000

    created format date file including the function

---
+++ app/services/utils/FormatDate.js
@@ -0,0 +1,11 @@
+// utils/formatDate.js
+module.exports = function formatDate(dateString) {
+    if (!dateString) return "N/A";
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  };
+  
