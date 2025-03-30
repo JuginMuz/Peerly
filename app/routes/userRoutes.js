09fa9e721f2e57a665bd7480cbd0874a82b70a52
KumarS14 <sonjaykumar1404@gmail.com>
2025-03-30 15:04:03 +0100

    imported post controller and made new routes so nessacary funcs needed for settings functionality can work

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,12 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
-
+const PostController = require('../controllers/PostController');
 // router.post('/register', UserController.register);
 // router.post('/login', UserController.login);
 // router.patch('/:user_id', UserController.updateProfile);
 // router.delete('/:user_id', UserController.deleteAccount);
 router.get('/:user_id', UserController.getProfile);
 router.get('/', UserController.getAllUsers);
+// New routes for settings page functionality
+router.post('/:user_id/updateProfile', UserController.updateProfile);
+router.post('/:user_id/deleteAccount', UserController.deleteAccount);
+router.post('/:user_id/deletePost/:post_id', PostController.deletePost);
+
+router.get('/:user_id', UserController.getProfile);
+router.get('/', UserController.getAllUsers);
+
+router.get('/:user_id/settings', UserController.getSettingsPage);
 
 module.exports = router;
