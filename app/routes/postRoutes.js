085ade4c666bc419fd0e38763e8b7d3774d0d732
Gurkiratjot17 <gurkiratjotsingh17@gmail.com>
2025-04-05 20:07:35 +0100

    introduced routes for creating post which facilitates image upload

--- app/routes/postRoutes.js
+++ app/routes/postRoutes.js
@@ -2,19 +2,21 @@
 const express = require('express');
 const router = express.Router();
 const PostController = require('../controllers/PostController');
+const { postUpload } = require('../models/upload'); 
 
 // router.post('/create', PostController.createPost);
 // router.delete('/:post_id', PostController.deletePost);
 
 router.get('/:post_id', PostController.getPostDetails);
 router.get('/', PostController.getAllPosts);
-router.get('/create/Post', (req, res) => {
-    // Render form for creating a post
-    res.render('create_post'); 
-  });
+router.get('/create/Post', PostController.showCreateForm);
+
   
-router.post('/create/NewPost', PostController.createPost);
+router.post('/create/NewPost', postUpload.single('postImage'), PostController.createPost);
 router.get('/posts/search', PostController.search);
+
+router.post('/:post_id/like', PostController.toggleLike);
+
 
 
   
